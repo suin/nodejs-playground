40c0b444b47992e9a503aaf3a5b831317cbfa01b
suin <suinyeze@gmail.com>
2020-10-08 12:12:26 +0900

    feat: 🎸 add deepclone benchmark

---
+++ deepclone/benchmark-nest32.js
@@ -0,0 +1,37 @@
+const Benchmark = require('benchmark')
+const report = require('beautify-benchmark')
+const v8 = require('v8')
+const _ = require('lodash')
+const deepcopy = require('deepcopy')
+const clone = require('clone')
+const cloneDeep = require('clone-deep')
+const rfdc = require('rfdc')()
+
+let node = {}
+const value = node
+for (let i = 0; i < 32; i++) {
+  node.child = {}
+  node = node.child
+}
+console.log(value)
+
+const json = () => JSON.parse(JSON.stringify(value))
+const v8_ = () => v8.deserialize(v8.serialize(value))
+const lodash = () => _.cloneDeep(value)
+const deepcopy_ = () => deepcopy(value)
+const clone_ = () => clone(value)
+const cloneDeep_ = () => cloneDeep(value)
+const rfdc_ = () => rfdc(value)
+
+const suite = new Benchmark.Suite()
+suite
+  .add(`json`, json)
+  .add(`v8`, v8_)
+  .add(`lodash`, lodash)
+  .add(`deepcopy`, deepcopy_)
+  .add(`clone`, clone_)
+  .add('clone-deep', cloneDeep_)
+  .add('rfdc', rfdc_)
+  .on('cycle', event => report.add(event.target))
+  .on('complete', () => report.log())
+  .run()
